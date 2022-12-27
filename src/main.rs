2cb105d26a50572a2bf0801f27602b5f75e462d6
Cynthia <josephao424@gmail.com>
2022-12-27 11:58:08 -0500

    A

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json::{Value, json};
+use serde_json::{json, Value};
 use shaco::rest;
 
 #[tokio::main]
@@ -10,6 +10,54 @@ async fn main() {
         "selectedPerkIds":  [8135, 8120, 8126, 8112, 8306, 8321]
     });
     push_runes_to_client(page).await;
+
+    let page = json!(
+        {
+          "associatedChampions": [
+            
+          ],
+          "associatedMaps": [
+            
+          ],
+          "blocks": [
+            {
+              "items": [
+                {
+                  "count": 1,
+                  "id": "3153"
+                },
+                {
+                  "count": 1,
+                  "id": "6673"
+                },
+                {
+                  "count": 1,
+                  "id": "3006"
+                },
+                {
+                  "count": 1,
+                  "id": "3091"
+                },
+                {
+                  "count": 1,
+                  "id": "3085"
+                },
+                {
+                  "count": 1,
+                  "id": "3072"
+                },
+                {
+                  "count": 1,
+                  "id": "3363"
+                }
+              ],
+              "type": "Final Build"
+            }
+          ],
+          "title": "Ahri Build",
+        }
+    );
+    push_items_to_client(page).await;
 }
 
 /// Attempts to push runes to the LoL Client Via the LCU API
@@ -39,12 +87,49 @@ pub async fn push_runes_to_client(page: Value) -> i64 {
                     return 1;
                 } else {
                     panic!()
-                    }
+                }
             } else {
                 panic!()
             }
         } else {
             panic!()
+        }
+    } else {
+        panic!()
+    }
+}
+
+/// Attempts to push an item set to the client via the LCU API
+/// this will eventually end up wrapped in some form of struct
+/// that handles checking if the LCU is open
+/// 
+/// Requires JSON as an argument
+pub async fn push_items_to_client(page: Value) -> i64 {
+    if let Ok(client) = rest::RESTClient::new() {
+        let a = client
+            .get("/lol-summoner/v1/current-summoner".to_owned())
+            .await;
+        match a {
+            Ok(a) => {
+                println!("{}", a);
+                let endpoint_a = format!("/lol-item-sets/v1/item-sets/{}/sets", a["summonerId"]);
+                let a = client.get(endpoint_a.clone()).await;
+                match a {
+                    Ok(json) => {
+                        let mut a = json.clone();
+                        a["itemSets"].as_array_mut().unwrap().push(page);
+                        let a = client.put(endpoint_a, a).await;          
+                        println!("{:?}", a);
+                        return 1;
+
+                    },
+                    Err(_) => panic!(),
+                }
+            }
+            Err(a) => {
+                println!("{}", a);
+                panic!()
+            }
         }
     } else {
         panic!()
