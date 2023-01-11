ae7ff561d3974cec9ccad3f16f86c55f5a362675
Cynthia <josephao424@gmail.com>
2023-01-11 18:45:34 -0500

    Update Shaco Tests

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() {
         "subStyleId": 8300,
         "selectedPerkIds":  [8135, 8120, 8126, 8112, 8306, 8321]
     });
-    push_runes_to_client(page).await;
+    println!("{}", push_runes_to_client(page).await);
 
     let page = json!(
         {
@@ -68,23 +68,18 @@ async fn main() {
 pub async fn push_runes_to_client(page: Value) -> i64 {
     let pages_endpoint = String::from("/lol-perks/v1/pages");
     if let Ok(client) = rest::RESTClient::new() {
-        if client
-            .put(pages_endpoint.clone(), page.clone())
-            .await
-            .is_ok()
-        {
-            return 1;
-        } else if let Ok(response) = client.get("/lol-perks/v1/currentpage".to_string()).await {
+        if let Ok(response) = client.get("/lol-perks/v1/currentpage".to_string()).await {
             let Some(id) = &response["id"].as_i64() else {
                 panic!();
             };
+            println!("{}", id);
             if client
-                .delete(format!("{0}/{1}", pages_endpoint, id))
+                .delete(format!("/lol-perks/v1/page/{}", id))
                 .await
                 .is_ok()
             {
-                if client.put(pages_endpoint, page).await.is_ok() {
-                    return 1;
+                if client.post(pages_endpoint, page).await.is_ok() {
+                    return 2;
                 } else {
                     panic!()
                 }
